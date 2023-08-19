05623b9a29e7ab152f2254fce6ea9994936d6f87
Stephen Kuenzli <skuenzli@k9security.io>
2023-08-18 17:12:29 -0700

    Integrate and implement a functioning Authorizer using Cedar Policy.

--- authorizer.py
+++ authorizer.py
@@ -1,8 +1,8 @@
 # The authorizer module implements an AWS API Gateway Lambda Authorizer using the Cedar Policy Framework
 # Introduction to Lambda Authorizers:
 #   https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-use-lambda-authorizer.html
-import json
 import logging
+from typing import List
 
 import cedarpy
 
@@ -15,14 +15,36 @@ def _log_function_execution_metadata(event: dict, context: dict) -> None:
     helpers.log_function_execution_metadata(logger, event, context)
 
 
-def authorize(event: dict, context: dict) -> dict:
-    logger.info(f'authorizing access for event: {event}')
+def is_authenticated(user_id, authorization_val):
+    if 'Bearer open-sesame' == authorization_val:
+        return True
+    else:
+        return False
 
-    _log_function_execution_metadata(event, context)
-    req_body = json.loads(event["body"])
-    user_id = req_body.get("userId", None)
-    photo_id = req_body.get("photoId", None)
 
+def authorize(authz_e: dict, context: dict) -> dict:
+    _log_function_execution_metadata(authz_e, context)
+
+    if 'REQUEST' == authz_e.get('type', None):
+        headers = authz_e.get('headers', {})
+        path_params = authz_e.get('pathParameters', {})
+    else:
+        logger.info(f'authz_e type is not REQUEST')
+        return _make_apig_authz_response('unsupported-request-type', 'Deny', '*')
+
+    user_id = path_params.get("userId", None)
+    photo_id = path_params.get("photoId", None)
+
+    if user_id is None or photo_id is None:
+        logger.info(f'authz_e missing userId or photoId path parameter')
+        return _make_apig_authz_response('unknown-principal-or-resource', 'Deny', '*')
+
+    if not is_authenticated(user_id, headers.get('Authorization', None)):
+        logger.info(f'could not authenticate caller')
+        return _make_apig_authz_response('unauthenticated-principal', 'Deny', '*')
+
+    # ok, we've authenticated the principal.  are they authorized?
+    
     principal = f'User::"{user_id}"'
     action = f'Action::"view"'
     resource = f'Photo::"{photo_id}"'
@@ -33,13 +55,137 @@ def authorize(event: dict, context: dict) -> dict:
         "resource": resource,
         "context": {}
     }
-    authz_result = cedarpy.is_authorized(request, "", "")
+    logger.info(f"authorizing request: {request}")
+    authz_result = cedarpy.is_authorized(request, _get_policies(), _get_entities())
+    logger.info(f"authz_result decision: {authz_result.decision.value} metrics: {authz_result.metrics}")
+
+    # Generate Lambda Authorizer response
+    # https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-lambda-authorizer-output.html
+
+    effect = "Deny"
     if cedarpy.Decision.Allow == authz_result.decision:
-        response = {
-            # build allow
+        effect = "Allow"
+
+    response = {
+        "principalId": user_id,
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": effect,
+                    "Resource": authz_e.get('methodArn', 'method-resource-missing')
+                }
+            ]
         }
-    else:
-        response = {
-            # deny
-        }
+    }
+
     return response
+
+
+def _get_policies() -> str:
+    common_policies = """
+            permit(
+                principal, 
+                action == Action::"view", 
+                resource
+            )
+            when {
+               resource.account == principal
+            };                
+            permit(
+                principal,
+                action == Action::"delete",
+                resource
+            )
+            when {
+                context.authenticated == true
+                &&
+                resource has account && principal == resource.account.owner
+            }
+            ;
+
+    """.strip()
+    return common_policies
+
+
+def _get_entities() -> List[dict]:
+    entities: List[dict] = [
+        {
+            "uid": {
+                "__expr": "User::\"alice\""
+            },
+            "attrs": {},
+            "parents": []
+        },
+        {
+            "uid": {
+                "__expr": "User::\"bob\""
+            },
+            "attrs": {},
+            "parents": []
+        },
+        {
+            "uid": {
+                "__expr": 'User::"skuenzli"'
+            },
+            "attrs": {},
+            "parents": []
+        },
+        {
+            "uid": {
+                "__expr": "Photo::\"bobs-photo-1\""
+            },
+            "attrs": {
+                "account": {"__expr": "User::\"bob\""}
+            },
+            "parents": []
+        },
+        {
+            "uid": {
+                "__expr": 'Photo::"kitt.png"'
+            },
+            "attrs": {
+                "account": {"__expr": 'User::"skuenzli"'}
+            },
+            "parents": []
+        },
+        {
+            "uid": {
+                "__expr": "Action::\"view\""
+            },
+            "attrs": {},
+            "parents": []
+        },
+        {
+            "uid": {
+                "__expr": "Action::\"edit\""
+            },
+            "attrs": {},
+            "parents": []
+        },
+        {
+            "uid": {
+                "__expr": "Action::\"delete\""
+            },
+            "attrs": {},
+            "parents": []
+        }
+    ]
+    return entities
+
+
+def _make_apig_authz_response(principal_id: str, effect: str, resource: str) -> dict:
+    return {
+            "principalId": principal_id,
+            "policyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Action": "execute-api:Invoke",
+                        "Effect": effect,
+                        "Resource": resource
+                    }
+                ]
+            }
+        }
---
+++ tests/unit/test_authorizer.py
@@ -0,0 +1,84 @@
+import unittest
+
+from authorizer import authorize
+
+
+class AuthorizerTestCase(unittest.TestCase):
+
+    def test_authorize_denies_invalid_requests(self):
+        for critical_member in ['type', 'headers', 'pathParameters']:
+            authz_e = self.make_valid_authz_request()
+            del authz_e[critical_member]
+
+            response = authorize(authz_e, {})
+            self.assertEqual("Deny", response["policyDocument"]["Statement"][0]["Effect"])
+
+    def test_authorizes_a_valid_request_for_authorized_resource(self):
+        authz_e = self.make_valid_authz_request()
+
+        response = authorize(authz_e, {})
+        self.assertEqual("Allow", response["policyDocument"]["Statement"][0]["Effect"])
+        self.assertEqual(authz_e['methodArn'],
+                         response["policyDocument"]["Statement"][0]["Resource"])
+
+    # noinspection PyMethodMayBeStatic
+    def make_valid_authz_request(self):
+        return {
+            'type': 'REQUEST',
+            'methodArn': 'arn:aws:execute-api:us-east-1:139710491120:vr3d61pcqe/dev-main/GET/users/skuenzli/photos/kitt.png',
+            'resource': '/users/{userId}/photos/{photoId}',
+            'path': '/users/skuenzli/photos/kitt.png',
+            'httpMethod': 'GET',
+            'headers': {'Accept': 'image/png',
+                        'Authorization': 'Bearer open-sesame',
+                        'CloudFront-Forwarded-Proto': 'https',
+                        'CloudFront-Is-Desktop-Viewer': 'true',
+                        'CloudFront-Is-Mobile-Viewer': 'false',
+                        'CloudFront-Is-SmartTV-Viewer': 'false',
+                        'CloudFront-Is-Tablet-Viewer': 'false',
+                        'CloudFront-Viewer-ASN': '22773', 'CloudFront-Viewer-Country': 'US',
+                        'Host': 'vr3d61pcqe.execute-api.us-east-1.amazonaws.com',
+                        'User-Agent': 'curl/7.88.1',
+                        'Via': '2.0 efdd98c65d70c59c49dba4d718c25274.cloudfront.net (CloudFront)',
+                        'X-Amz-Cf-Id': 'ODZuEV6Oj-B_9tP7LZqqawV0bV2KWuALJiCclHnAVyyQjXnF-gAfTA==',
+                        'X-Amzn-Trace-Id': 'Root=1-64e0039e-25a58e2f21498a1812ae6839',
+                        'X-Forwarded-For': '98.177.1.4, 52.46.35.86',
+                        'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https'},
+            'multiValueHeaders': {
+                'Accept': ['image/png'],
+                'Authorization': ['Bearer open-sesame'],
+                'CloudFront-Forwarded-Proto': ['https'], 'CloudFront-Is-Desktop-Viewer': ['true'],
+                'CloudFront-Is-Mobile-Viewer': ['false'],
+                'CloudFront-Is-SmartTV-Viewer': ['false'],
+                'CloudFront-Is-Tablet-Viewer': ['false'], 'CloudFront-Viewer-ASN': ['22773'],
+                'CloudFront-Viewer-Country': ['US'],
+                'Host': ['vr3d61pcqe.execute-api.us-east-1.amazonaws.com'],
+                'User-Agent': ['curl/7.88.1'],
+                'Via': ['2.0 efdd98c65d70c59c49dba4d718c25274.cloudfront.net (CloudFront)'],
+                'X-Amz-Cf-Id': ['ODZuEV6Oj-B_9tP7LZqqawV0bV2KWuALJiCclHnAVyyQjXnF-gAfTA=='],
+                'X-Amzn-Trace-Id': ['Root=1-64e0039e-25a58e2f21498a1812ae6839'],
+                'X-Forwarded-For': ['98.177.1.4, 52.46.35.86'], 'X-Forwarded-Port': ['443'],
+                'X-Forwarded-Proto': ['https']},
+            'queryStringParameters': {},
+            'multiValueQueryStringParameters': {},
+            'pathParameters': {'photoId': 'kitt.png', 'userId': 'skuenzli'},
+            'stageVariables': {},
+            'requestContext': {
+                'resourceId': '2g7hdj',
+                'resourcePath': '/users/{userId}/photos/{photoId}',
+                'httpMethod': 'GET', 'extendedRequestId': 'J4WAxEudIAMFX3Q=',
+                'requestTime': '18/Aug/2023:23:49:50 +0000',
+                'path': '/dev-main/users/skuenzli/photos/kitt.png', 'accountId': '139710491120',
+                'protocol': 'HTTP/1.1', 'stage': 'dev-main', 'domainPrefix': 'vr3d61pcqe',
+                'requestTimeEpoch': 1692402590285,
+                'requestId': 'e4e9d76f-8d5a-49dd-9083-f5015318c292',
+                'identity': {'cognitoIdentityPoolId': None, 'accountId': None,
+                             'cognitoIdentityId': None, 'caller': None, 'sourceIp': '98.177.1.4',
+                             'principalOrgId': None, 'accessKey': None,
+                             'cognitoAuthenticationType': None,
+                             'cognitoAuthenticationProvider': None, 'userArn': None,
+                             'userAgent': 'curl/7.88.1', 'user': None},
+                'domainName': 'vr3d61pcqe.execute-api.us-east-1.amazonaws.com',
+                'apiId': 'vr3d61pcqe'
+            }
+        }
